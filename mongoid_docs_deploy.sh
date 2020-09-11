4df34dca3cd8d11e62805ff556a410d5718538d1
Chris Cho <chris.cho@10gen.com>
2020-09-11 14:16:05 -0400

    deploy script for mongoid docs

---
+++ mongoid_docs_deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# version branch names in descending order
+VERSIONS=(master 7.1 7.0 6.4 6.3 6.2 6.1 5.4)
+
+# clean
+git checkout master
+make clean
+
+# build each version in order
+for i in "${VERSIONS[@]}"; do
+  echo "Branch [$i]: Generating HTML and local directories"
+  git checkout $i && git pull && make html publish
+done
+
+# Prompt in case errors encountered
+read -p "Proceed with deploy? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+# deploy
+for i in "${VERSIONS[@]}"; do
+  echo "Deploying [$i]"
+  git checkout $i && yes | make deploy
+done
+
+echo "Deployment complete!"
