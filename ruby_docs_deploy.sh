2d4f6583b1ecc5d5e126d3780e9f78d62939b409
Nathan Leniz <nathan.leniz@mongodb.com>
2021-07-15 10:00:08 -0700

    add ruby version

--- ruby_docs_deploy.sh
+++ ruby_docs_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # version branch names in descending order
-VERSIONS=(master v2.14 v2.13 v2.12 v2.11 v2.10 v2.9 v2.8 v2.7 v2.6 v2.5 v2.4 v2.3 v2.2 v2.0 v1.x)
+VERSIONS=(master v2.15 v2.14 v2.13 v2.12 v2.11 v2.10 v2.9 v2.8 v2.7 v2.6 v2.5 v2.4 v2.3 v2.2 v2.0 v1.x)
 
 # clean
 git checkout master
