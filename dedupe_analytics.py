197d01fb28b842d8eecb18e2b069eb9945497f9e
Chris Cho <chris.cho@10gen.com>
2022-02-23 13:25:12 -0500

    add dedupe analytics script

---
+++ dedupe_analytics.py
@@ -0,0 +1,32 @@
+import csv
+import re
+import collections
+
+# This script removes version from docs URLs and aggregates the unique page view totals
+
+# drivers
+#URL_VERSION_RE = r"^docs\.mongodb\.com\/drivers\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
+
+# connectors
+#URL_VERSION_RE = r"^docs\.mongodb\.com\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
+
+# GH pages
+URL_VERSION_RE = r"^mongodb\.github\.io\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
+
+with open('report.csv') as csvfile:
+
+    results = {}
+
+    fh = csv.DictReader(csvfile, delimiter=',')
+    for row in fh:
+        matches = re.match(URL_VERSION_RE, row['Page']);
+
+        if matches:
+
+            url = "%s/%s" % (matches.group(1), matches.group(3))
+            total = int(row['Unique Pageviews'])
+
+            results[url] = results.get(url, 0) + total
+    sorted_results = collections.OrderedDict(sorted(results.items(), key=lambda item: -item[1]))
+    for k,v in sorted_results.items():
+        print("%s,%s" % (k,v))
