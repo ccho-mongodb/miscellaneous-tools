156df1cf9b9560f413ab8aa5468da224874a660d
Chris Cho <chris.cho@mongodb.com>
2022-07-22 14:25:28 -0400

    Skunk2022 test PR

--- dedupe_analytics.py
+++ dedupe_analytics.py
@@ -4,6 +4,9 @@ import collections
 
 # This script removes version from docs URLs and aggregates the unique page view totals
 
+# server
+URL_VERSION_RE = "(www\.mongodb\.com\/docs)\/(upcoming|current|v[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
+
 # drivers
 #URL_VERSION_RE = r"^docs\.mongodb\.com\/drivers\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
 
@@ -11,7 +14,7 @@ import collections
 #URL_VERSION_RE = r"^docs\.mongodb\.com\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
 
 # GH pages
-URL_VERSION_RE = r"^mongodb\.github\.io\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
+#URL_VERSION_RE = r"^mongodb\.github\.io\/([\-\/\w+]*)/(current?|[0-9]+\.[0-9]+?)\/([#\?\-\/\w+]*)"
 
 with open('report.csv') as csvfile:
 
@@ -22,11 +25,11 @@ with open('report.csv') as csvfile:
         matches = re.match(URL_VERSION_RE, row['Page']);
 
         if matches:
-
             url = "%s/%s" % (matches.group(1), matches.group(3))
-            total = int(row['Unique Pageviews'])
-
+            total = int(row['Unique_Pageviews'])
             results[url] = results.get(url, 0) + total
+
+
     sorted_results = collections.OrderedDict(sorted(results.items(), key=lambda item: -item[1]))
     for k,v in sorted_results.items():
         print("%s,%s" % (k,v))
