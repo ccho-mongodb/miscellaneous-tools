c395d8f260413e5aa2b6a867287e5b8808b6c4b5
Chris Cho <chris.cho@mongodb.com>
2023-03-02 14:02:01 -0500

    alas but one

---
+++ alas_but_one.py
@@ -0,0 +1,39 @@
+import os
+import re
+from collections import Counter
+
+# Usage
+# -----
+# This script reads files in the relative "source/" directory and sub-directories
+# named with the "rst" or "txt" extension and counts the instances of each
+# word (as matched by the WORD_REGEX constant).
+# Update the MAX_OCCUR=n constant to print out only words that are repeated
+# up to n times.
+#
+# Backstory
+# ---------
+# A user reported an issue with documentation in which "Atlas" was misspelled 
+# as "Alas". After investigation, a writer reported there was only one instance 
+# of the type. Hence, "there was, alas, but one".
+
+MAX_OCCUR=1
+WORD_REGEX=re.compile("[\w'_\-]+")
+
+directory = 'source'
+filename_re = r'\.(txt|rst)$'
+
+word_list = []
+for root, dirs, files in os.walk(directory):
+    for filename in files:
+        if re.search(filename_re, filename):
+            filepath = os.path.join(root, filename)
+            try:
+                word_list += re.findall(WORD_REGEX, open(os.path.join(root, filename), 'r', encoding='utf-8').read().lower())
+            except Exception as e:
+                print(e)
+
+ctr = Counter(word_list)
+for w,v in sorted(ctr.items(), key=lambda pair: pair[1], reverse=True):
+    if (v <= MAX_OCCUR):
+        print("%s [%d]" % (w,v))
+
