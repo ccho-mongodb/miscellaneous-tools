e1e32a373e3624fbbf9e9d29b533650c3e732a09
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-04-25 10:54:01 -0400

    CC suggestions

--- php_docs_deploy.sh
+++ php_docs_deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # version branch names in descending order
-VERSIONS=(master v1.15 v1.14 v1.13 v1.12 v1.11 v1.10 v1.9 v1.8 v1.7 v1.6 v1.5 v1.4 v1.3 v1.2 v1.1)
+VERSIONS=(master v1.15 v1.13 v1.12 v1.11 v1.10 v1.9 v1.8 v1.7 v1.6 v1.5 v1.4 v1.3 v1.2 v1.1)
 
 # clean
 git checkout master
-make clean
 
 TODEPLOY=()
 for i in "${VERSIONS[@]}"
