93489ea0ee97e0347f1e1266435007651a3d8ad3
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-05-01 11:31:14 -0400

    cc suggestions

--- alas_but_one.py
+++ alas_but_one.py
@@ -1,28 +1,38 @@
 import os
 import re
 from spellchecker import SpellChecker
-
 from collections import defaultdict
 import string
+import argparse
 
 # Usage
 # -----
 # This script reads files in the relative "source/" directory and sub-directories
-# named with the "rst" or "txt" extension and counts the instances of each
-# word.
-# Update the occur_freq=n constant to print out only words that are repeated
-# up to n times.
+# named with the "rst" or "txt" extension and outputs a list of words that occur
+# only at a frequency that you specify. By default, the script runs a
+# spellchecker on the output list and separates the list
+# into words that are spelled correctly and words that are potentially misspelled.
 #
 # Backstory
 # ---------
 # A user reported an issue with documentation in which "Atlas" was misspelled
 # as "Alas". After investigation, a writer reported there was only one instance
 # of the type. Hence, "there was, alas, but one".
+#
+# Instructions
+# ------------
+# Run this file at the same level as your "/source" directory.
+# Update the OCCUR_FREQ=n constant in the main method to locate only words that are repeated
+# up to n times.
+# When you run the file, you can include the optional "--noSpellCheck"
+# flag to disable the spellchecker function:
+# e.g. python3 alas_but_one.py --noSpellCheck > output.txt
 
 
 def count_in_dir():
     directory = "source"
     filename_re = r"\.(txt|rst)$"
+    pattern = re.compile(r"\s*(?:[()]|'s|\s|-|`|/|#)\s*")
 
     processed = []
     words_and_freqs = []
@@ -37,7 +47,7 @@ def count_in_dir():
 
                 for line in read_file:
                     line = line.strip()
-                    words = re.split(r"\s*(?:[()]|'s|\s|-|`|/|#)\s*", line)
+                    words = pattern.split(line)
 
                     for word in words:
                         pword = word.strip(string.punctuation)
@@ -55,30 +65,43 @@ def count_in_dir():
 
 
 def num_occurrences(dict, n):
-    occurs_n_times = []
+    occurs_upto_n_times = []
     for key in dict.keys():
-        if dict[key] == n:
-            occurs_n_times.append(key)
+        if dict[key] <= n:
+            occurs_upto_n_times.append(key)
 
-    return occurs_n_times
+    return occurs_upto_n_times
 
 
 def main():
-    spell = SpellChecker()
+    parser = argparse.ArgumentParser()
     x = count_in_dir()
 
-    occur_freq = 1
-    lst = num_occurrences(x, occur_freq)
-    spelled = spell.known(lst)
+    OCCUR_FREQ = 1
+    lst = num_occurrences(x, OCCUR_FREQ)
 
-    header_occ1 = "Words that occur only {}x, but spelled correctly:".format(occur_freq)
-    print("\n" + header_occ1)
-    print(len(header_occ1) * "=", *spelled, sep="\n- ")
+    parser.add_argument("--noSpellCheck", action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
 
-    misspelled = spell.unknown(lst)
-    header_misspelled = "Words that occur only {}x and misspelled:".format(occur_freq)
-    print("\n" + header_misspelled)
-    print(len(header_misspelled) * "=", *misspelled, sep="\n- ")
+    if args.noSpellCheck:
+        header_nocheck = "Words that occur up to {}x:".format(OCCUR_FREQ)
+        print("\n" + header_nocheck)
+        print(len(header_nocheck) * "=", *lst, sep="\n- ")
+    else:
+        spell = SpellChecker()
+        spelled = spell.known(lst)
+        header_occ1 = "Words that occur up to {}x, but spelled correctly:".format(
+            OCCUR_FREQ
+        )
+        print("\n" + header_occ1)
+        print(len(header_occ1) * "=", *spelled, sep="\n- ")
+
+        misspelled = spell.unknown(lst)
+        header_misspelled = "Words that occur up to {}x and are misspelled:".format(
+            OCCUR_FREQ
+        )
+        print("\n" + header_misspelled)
+        print(len(header_misspelled) * "=", *misspelled, sep="\n- ")
 
 
 if __name__ == "__main__":
