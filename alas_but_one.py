bdfd8d5b2253cbde6bde8bd8796e00ec3df02141
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-05-01 10:11:58 -0400

    Rea improve alas but one script

--- alas_but_one.py
+++ alas_but_one.py
@@ -1,39 +1,85 @@
 import os
 import re
-from collections import Counter
+from spellchecker import SpellChecker
+
+from collections import defaultdict
+import string
 
 # Usage
 # -----
 # This script reads files in the relative "source/" directory and sub-directories
 # named with the "rst" or "txt" extension and counts the instances of each
-# word (as matched by the WORD_REGEX constant).
-# Update the MAX_OCCUR=n constant to print out only words that are repeated
+# word.
+# Update the occur_freq=n constant to print out only words that are repeated
 # up to n times.
 #
 # Backstory
 # ---------
-# A user reported an issue with documentation in which "Atlas" was misspelled 
-# as "Alas". After investigation, a writer reported there was only one instance 
+# A user reported an issue with documentation in which "Atlas" was misspelled
+# as "Alas". After investigation, a writer reported there was only one instance
 # of the type. Hence, "there was, alas, but one".
 
-MAX_OCCUR=1
-WORD_REGEX=re.compile("[\w'_\-]+")
 
-directory = 'source'
-filename_re = r'\.(txt|rst)$'
+def count_in_dir():
+    directory = "source"
+    filename_re = r"\.(txt|rst)$"
 
-word_list = []
-for root, dirs, files in os.walk(directory):
-    for filename in files:
-        if re.search(filename_re, filename):
-            filepath = os.path.join(root, filename)
-            try:
-                word_list += re.findall(WORD_REGEX, open(os.path.join(root, filename), 'r', encoding='utf-8').read().lower())
-            except Exception as e:
-                print(e)
+    processed = []
+    words_and_freqs = []
 
-ctr = Counter(word_list)
-for w,v in sorted(ctr.items(), key=lambda pair: pair[1], reverse=True):
-    if (v <= MAX_OCCUR):
-        print("%s [%d]" % (w,v))
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if re.search(filename_re, filename):
+                filepath = os.path.join(root, filename)
+                # print(filepath)
+                read_file = open(filepath, "r", encoding="utf-8")
+                # processed = []
 
+                for line in read_file:
+                    line = line.strip()
+                    words = re.split(r"\s*(?:[()]|'s|\s|-|`|/|#)\s*", line)
+
+                    for word in words:
+                        pword = word.strip(string.punctuation)
+                        processed.append(pword.lower())
+
+    for elem in processed:
+        if elem != None:
+            words_and_freqs.append(elem)
+
+    freq = defaultdict(int)
+    for item in words_and_freqs:
+        freq[item] += 1
+
+    return freq
+
+
+def num_occurrences(dict, n):
+    occurs_n_times = []
+    for key in dict.keys():
+        if dict[key] == n:
+            occurs_n_times.append(key)
+
+    return occurs_n_times
+
+
+def main():
+    spell = SpellChecker()
+    x = count_in_dir()
+
+    occur_freq = 1
+    lst = num_occurrences(x, occur_freq)
+    spelled = spell.known(lst)
+
+    header_occ1 = "Words that occur only {}x, but spelled correctly:".format(occur_freq)
+    print("\n" + header_occ1)
+    print(len(header_occ1) * "=", *spelled, sep="\n- ")
+
+    misspelled = spell.unknown(lst)
+    header_misspelled = "Words that occur only {}x and misspelled:".format(occur_freq)
+    print("\n" + header_misspelled)
+    print(len(header_misspelled) * "=", *misspelled, sep="\n- ")
+
+
+if __name__ == "__main__":
+    main()
