879ccd1f5386c921d74c0cbe495732ea36d07aaa
Chris Cho <chris.cho@10gen.com>
2023-06-23 14:37:02 -0400

    Update ruby_docs_deploy.sh

--- ruby_docs_deploy.sh
+++ ruby_docs_deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 # version branch names in descending order
-VERSIONS=(master v2.15 v2.14 v2.13 v2.12 v2.11 v2.10 v2.9 v2.8 v2.7 v2.6 v2.5 v2.4 v2.3 v2.2 v2.0 v1.x)
+VERSIONS=(master v2.19 v2.18 v2.17 v2.16 v2.15 v2.14 v2.13 v2.12 v2.11 v2.10 v2.9 v2.8 v2.7 v2.6 v2.5 v2.4 v2.3 v2.2 v2.0 v1.x)
 
 # clean
 git checkout master
-make clean
+
+TODEPLOY=()
+for i in "${VERSIONS[@]}"
+do
+    read -p "add branch [$i]? [y/n] " REPLY
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        TODEPLOY+=($i)
+    fi
+done
 
 # build each version in order
-for i in "${VERSIONS[@]}"; do
+for i in "${TODEPLOY[@]}"; do
     echo "Branch [$i]: Generating HTML and local directories"
     git checkout $i && git pull && make html publish
 done
@@ -21,7 +29,7 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 fi
 
 # deploy
-for i in "${VERSIONS[@]}"; do
+for i in "${TODEPLOY[@]}"; do
     echo "Deploying [$i]"
     git checkout $i && yes | make deploy
 done
